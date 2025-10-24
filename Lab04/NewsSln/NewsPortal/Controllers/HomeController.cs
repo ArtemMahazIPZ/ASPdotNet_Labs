e2c1365df1e6029869bcbfcb994737ebeb4ada4b
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2025-10-24 23:55:24 +0300

    Delete Lab04 directory

--- Lab04/NewsSln/NewsPortal/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace NewsPortal.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index() => View();
-    }
-}
